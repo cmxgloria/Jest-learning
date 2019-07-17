171fb44c34ff38811a51fe036c8bd9c49016aa79
Gloria Chen <cmxgloria@gmail.com>
2019-07-17 11:38:46 +1000

    array sample

---
+++ JEST_TESTING_BASICS/chunk.js
@@ -0,0 +1,18 @@
+const chunkedArray = (arr, len) => {
+  //init chunked arr
+  const chunkedArr = [];
+  //loop through Arr
+  arr.forEach(val => {
+    //get last element
+    const last = chunkedArr[chunkedArr.length - 1];
+
+    //check if last and if last length is equal to the chunk len
+    if (!last || last.length === len) {
+      chunkedArr.push([val]);
+    } else {
+      last.push(val);
+    }
+  });
+  return chunkedArr;
+}
+module.exports = chunkedArray;
